7364f45f9baaa482eae212e5922b39f14d804d91
newkid004 <newkid004@gmail.com>
2019-06-12 21:10:15 +0900

    Update CDelegateHandler.h
    Use delegate like Action and Func in C#

--- CppDelegate/CDelegateHandler.h
+++ CppDelegate/CDelegateHandler.h
@@ -1,32 +1,92 @@
 #pragma once
 
-template<typename ...Types>
+template<typename Return, typename ...Types>
 class CDelegate;
 
-template<typename ...Types>
+template<typename Return, typename ...Types>
 class CDelegateHandler
 {
 private :
-	using OwnType = CDelegateHandler<Types...>;
+	using OwnType = CDelegateHandler<Return, Types...>;
 
 public:
-	friend class CDelegate<Types...>;
+	friend class CDelegate<Return, Types...>;
 
 public :
-	using FuncArgs = void(Types...);
-	using FuncType = std::function<void(Types...)>;
+	using FuncArgs = Return(Types...);
+	using FuncType = std::function<Return(Types...)>;
 
 private :
 	FuncType* _ManagedID = nullptr;
 	FuncType _Func;
 
 public :
-	void Invoke(const Types&... types)
+	Return Invoke(const Types&... types)
 	{
-		_Func(types...);
+		return _Func(types...);
 	}
 
 public :
 	CDelegateHandler(const FuncType& Func) : _Func(Func) { };
 	virtual ~CDelegateHandler() = default;
 };
+
+#pragma region CAction
+
+template<typename ...Types>
+class CDelegateHandler<void, Types...>
+{
+private:
+	using OwnType = CDelegateHandler<void, Types...>;
+
+public:
+	friend class CDelegate<void, Types...>;
+
+public:
+	using FuncArgs = void(Types...);
+	using FuncType = std::function<void(Types...)>;
+
+private:
+	FuncType* _ManagedID = nullptr;
+	FuncType _Func;
+
+public:
+	void Invoke(const Types& ... types)
+	{
+		_Func(types...);
+	}
+
+public:
+	CDelegateHandler(const FuncType& Func) : _Func(Func) { };
+	virtual ~CDelegateHandler() = default;
+};
+
+template<>
+class CDelegateHandler<void>
+{
+private:
+	using OwnType = CDelegateHandler<void>;
+
+public:
+	friend class CDelegate<void>;
+
+public:
+	using FuncArgs = void();
+	using FuncType = std::function<void()>;
+
+private:
+	FuncType* _ManagedID = nullptr;
+	FuncType _Func;
+
+public:
+	void Invoke()
+	{
+		_Func();
+	}
+
+public:
+	CDelegateHandler(const FuncType& Func) : _Func(Func) { };
+	virtual ~CDelegateHandler() = default;
+};
+
+#pragma endregion
