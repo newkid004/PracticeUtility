f07471d6f40479b51654336eae7cf7f860cb51ba
newkid004 <newkid004@gmail.com>
2021-07-30 03:09:10 +0900

    Update CustomRoutine.cs
    버그 수정, CallLate 내 종료 액션 추가

--- C#/Unity/CustomRoutine/CustomRoutine.cs
+++ C#/Unity/CustomRoutine/CustomRoutine.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 
 public static class CustomRoutine
 {
+	private static bool _bIsInit = false;
+
 	private static CustomRoutineManager _Routiner = null;
 	private static CustomRoutineManager Routiner
 	{
@@ -14,11 +16,13 @@ public static class CustomRoutine
 		{
 			if (_Routiner == null)
 			{
-				GameObject goRoutiner = new GameObject("CustomRoutiner");
-				GameObject.DontDestroyOnLoad(goRoutiner);
+				GameObject goRoutiner = new GameObject("CustomRoutinManager");
+				UnityEngine.Object.DontDestroyOnLoad(goRoutiner);
 
 				_Routiner = goRoutiner.AddComponent<CustomRoutineManager>();
 				_Routiner.Init();
+
+				_bIsInit = true;
 			}
 			return _Routiner;
 		}
@@ -28,29 +32,42 @@ public static class CustomRoutine
 	{
 		private Dictionary<int, int> _dictCoroutineIndex;
 		private Dictionary<int, Dictionary<int, Coroutine>> _dictManagementCoroutine;
+		private Dictionary<float, WaitForSeconds> _dictWaitForSeconds;
+		private Dictionary<float, WaitForSecondsRealtime> _dictWaitForSecondsRealtime;
+
 		private int _currentSceneIndex = -1;
 
-		private int NextCoroutineIndex { get => ++_dictCoroutineIndex[_currentSceneIndex]; }
-		private Dictionary<int, Coroutine> CurrentCoroutineDict { get => _dictManagementCoroutine[_currentSceneIndex]; }
+		private int NextCoroutineIndex 
+		{
+			get
+			{
+				int iIndex = _dictCoroutineIndex.GetDef(_currentSceneIndex);
+				return _dictCoroutineIndex.SetSafe(_currentSceneIndex, iIndex + 1);
+			}
+		}
+		private Dictionary<int, Coroutine> CurrentCoroutineDict { get => _dictManagementCoroutine.GetSafe(_currentSceneIndex); }
 
 		public void Init()
 		{
 			_dictCoroutineIndex = new Dictionary<int, int>();
 			_dictManagementCoroutine = new Dictionary<int, Dictionary<int, Coroutine>>();
+			_dictWaitForSeconds = new Dictionary<float, WaitForSeconds>();
+			_dictWaitForSecondsRealtime = new Dictionary<float, WaitForSecondsRealtime>();
+
 			_currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
-			SceneManager.activeSceneChanged += OnLevelWasLoaded;
+			SceneManager.sceneLoaded += sceneLoaded;
 		}
 
 		public void Dispose()
 		{
-			SceneManager.activeSceneChanged -= OnLevelWasLoaded;
+			SceneManager.sceneLoaded -= sceneLoaded;
 		}
 
-		private void OnLevelWasLoaded(Scene sceneBefore, Scene sceneAfter)
+		private void sceneLoaded(Scene stScene, LoadSceneMode eLoadSceneMode)
 		{
-			StopSceneRoutine(sceneBefore.buildIndex);
-			_currentSceneIndex = sceneAfter.buildIndex;
+			StopSceneRoutine(_currentSceneIndex);
+			_currentSceneIndex = stScene.buildIndex;
 		}
 
 		private void StopSceneRoutine(int index = -1)
@@ -58,53 +75,66 @@ public static class CustomRoutine
 			if (index < 0)
 				index = _currentSceneIndex;
 
-			foreach (var sceneRoutine in _dictManagementCoroutine[index])
-				StopCoroutine(sceneRoutine.Value);
+			Dictionary<int, Coroutine> dictManagementCoroutine = _dictManagementCoroutine.GetDef(index);
+			if (dictManagementCoroutine != null)
+			{
+				foreach (var sceneRoutine in _dictManagementCoroutine[index])
+					StopCoroutine(sceneRoutine.Value);
+			}
 		}
 
 		public void StopRoutine(int index)
 		{
-			CurrentCoroutineDict.Remove(index);
 			StopCoroutine(CurrentCoroutineDict[index]);
+			CurrentCoroutineDict.Remove(index);
 		}
 
-		public int CallLate(float delay, Action callback, bool isRealtime = false)
+		public int CallLate(float delay, Action actCallback, bool isRealtime = false, Action actOnEnd = null)
 		{
 			int routineIndex = NextCoroutineIndex;
 
-			Coroutine c = StartCoroutine(DoCallLate(delay, callback, isRealtime, routineIndex));
+			Coroutine c = StartCoroutine(DoCallLate(delay, actCallback, actOnEnd, isRealtime, routineIndex));
 			CurrentCoroutineDict.Add(routineIndex, c);
 
 			return routineIndex;
 		}
 
-		private IEnumerator DoCallLate(float delay, Action callback, bool isRealtime , int routineIndex)
+		private IEnumerator DoCallLate(float delay, Action actCallback, Action actOnEnd, bool isRealtime, int routineIndex)
 		{
 			int nowCurrentScene = _currentSceneIndex;
 
 			if (isRealtime)
-				yield return new WaitForSecondsRealtime(delay);
+			{
+				WaitForSecondsRealtime wfsObject = _dictWaitForSecondsRealtime.GetDef(delay);
+				yield return wfsObject != null ?
+					wfsObject : _dictWaitForSecondsRealtime.SetSafe(delay, new WaitForSecondsRealtime(delay));
+			}
 			else
-				yield return new WaitForSeconds(delay);
+			{
+				WaitForSeconds wfsObject = _dictWaitForSeconds.GetDef(delay);
+				yield return wfsObject != null ?
+					wfsObject : _dictWaitForSeconds.SetSafe(delay, new WaitForSeconds(delay));
+			}
 
 			if (nowCurrentScene == _currentSceneIndex)
 			{
-				callback();
+				actCallback();
+				actOnEnd?.Invoke();
 				CurrentCoroutineDict.Remove(routineIndex);
 			}
 		}
 
-		public int CallLoop(Func<bool> callback, Action OnEnd = null)
+		public int CallLoop(Func<bool> callback, Action actOnEnd = null)
 		{
 			int routineIndex = NextCoroutineIndex;
 
-			Coroutine c = StartCoroutine(DoCallLoop(callback, OnEnd, routineIndex));
+			Coroutine c = StartCoroutine(DoCallLoop(callback, actOnEnd, routineIndex));
 			CurrentCoroutineDict.Add(routineIndex, c);
 
 			return routineIndex;
 		}
 
-		private IEnumerator DoCallLoop(Func<bool> callback, Action OnEnd, int routineIndex)
+		private IEnumerator DoCallLoop(Func<bool> callback, Action actOnEnd, int routineIndex)
 		{
 			int nowCurrentScene = _currentSceneIndex;
 
@@ -118,21 +148,40 @@ public static class CustomRoutine
 
 			if (nowCurrentScene == _currentSceneIndex)
 			{
-				OnEnd?.Invoke();
+				actOnEnd?.Invoke();
 				CurrentCoroutineDict.Remove(routineIndex);
 			}
 		}
 	}
 
+	public static void Init()
+	{
+		if (false == _bIsInit)
+		{
+			if (_Routiner == null)
+			{
+				CustomRoutineManager refManager = Routiner;
+			}
+		}
+	}
+
+	public static void Dispose()
+	{
+		if (_bIsInit)
+		{
+			Routiner.Dispose();
+		}
+	}
+
 	public static void Stop(int routineIndex) => Routiner.StopRoutine(routineIndex);
-	public static int CallLate(float delay, Action callback, bool isRealtime = false) => Routiner.CallLate(delay, callback, isRealtime);
+	public static int CallLate(float delay, Action actCallback, bool isRealtime = false, Action actOnEnd = null) => Routiner.CallLate(delay, actCallback, isRealtime, actOnEnd);
 	public static int CallLoop(Func<bool> callback, Action OnEnd = null) => Routiner.CallLoop(callback, OnEnd);
 
-	public static int Pause(float Second, bool isRealtime = true)
-	{
-		float prevTime = Time.timeScale;
-		Time.timeScale = 0.0f;
-
-		return Routiner.CallLate(Second, () => Time.timeScale = prevTime, isRealtime);
-	}
+	// public static int Pause(float Second, bool isRealtime = true)
+	// {
+	// 	float prevTime = Time.timeScale;
+	// 	Time.timeScale = 0.0f;
+	// 
+	// 	return Routiner.CallLate(Second, () => Time.timeScale = prevTime, isRealtime);
+	// }
 } //
